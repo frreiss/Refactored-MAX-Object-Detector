64c9cc5cb0445eb8d3cf70f2d53747ff44171e6f
Daniel Jalova <djalova@us.ibm.com>
2019-05-29 16:40:31 -0700

    Update conversion flags

--- tensorflowjs/convert.py
+++ tensorflowjs/convert.py
@@ -1,20 +1,23 @@
 # import tensorflow as tf
 import tensorflowjs as tfjs
-from tensorflowjs.converters import tf_saved_model_conversion
+from tensorflowjs.converters import tf_saved_model_conversion_pb
 
-saved_model_path = "./saved_model_js"
-output_path = "tfjs_models"
+saved_model_path = "../saved_model_js"
+output_path = "web_model"
 
 def convert_to_tfjs(input_dir, output_dir):
+    # Reference: https://github.com/tensorflow/tfjs-converter/blob/0.8.x/python/tensorflowjs/converters/converter.py
     output_node_names = "detection_boxes,detection_classes,detection_scores,num_detections"
 
-    tf_saved_model_conversion.convert_tf_saved_model(input_dir, 
+    tf_saved_model_conversion_pb.convert_tf_saved_model(input_dir, 
         output_node_names,
         output_dir,
-        skip_op_check=True)
+        saved_model_tags='serve',
+        quantization_dtype=None,
+        skip_op_check=False,
+        strip_debug_ops=True)
 
 def main():
-    # Reference: https://github.com/tensorflow/tfjs-converter/blob/0.8.x/python/tensorflowjs/converters/converter.py
     convert_to_tfjs(saved_model_path, output_path)
 
 
